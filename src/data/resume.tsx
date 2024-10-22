122aa9234a65307d132751addc57934fe7db1153
shreyankjadiya120 <shreyankjadiya120@gmail.com>
2024-10-22 19:52:56 +0000

    Linkedin link fix

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -50,7 +50,7 @@ export const DATA = {
       },
       LinkedIn: {
         name: "LinkedIn",
-        url: "www.linkedin.com/in/shreyank-jadiya",
+        url: "https://www.linkedin.com/in/shreyank-jadiya",
         icon: Icons.linkedin,
         navbar: true,
       },
